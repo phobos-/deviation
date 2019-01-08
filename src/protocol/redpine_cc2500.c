c278adac5449c26619a59c448d2857f8ba1823e3
Bryce Johnson <bryce@raspberrywood.com>
2019-01-08 14:30:00 -0700

    Switch to use hexfet reduced latency mixer method Use crc from frsky_x

--- src/protocol/redpine_cc2500.c
+++ src/protocol/redpine_cc2500.c
@@ -71,11 +71,13 @@ static enum {
 #else
   REDPINE_BIND_DONE = 50,
 #endif
-  REDPINE_DATA1,
+  REDPINE_DATAM,
+  REDPINE_DATA1
 } state;
 
 static u16 fixed_id;
 static u8 packet[MAX_PACKET_SIZE];
+static u16 mixer_runtime;
 
 static const u8 hop_data[] = {
   0x02, 0xD4, 0xBB, 0xA2, 0x89,
@@ -91,49 +93,8 @@ static const u8 hop_data[] = {
 };
 
 
-static const u16 CRCTable[] = {
-  0x0000,0x1189,0x2312,0x329b,0x4624,0x57ad,0x6536,0x74bf,
-  0x8c48,0x9dc1,0xaf5a,0xbed3,0xca6c,0xdbe5,0xe97e,0xf8f7,
-  0x1081,0x0108,0x3393,0x221a,0x56a5,0x472c,0x75b7,0x643e,
-  0x9cc9,0x8d40,0xbfdb,0xae52,0xdaed,0xcb64,0xf9ff,0xe876,
-  0x2102,0x308b,0x0210,0x1399,0x6726,0x76af,0x4434,0x55bd,
-  0xad4a,0xbcc3,0x8e58,0x9fd1,0xeb6e,0xfae7,0xc87c,0xd9f5,
-  0x3183,0x200a,0x1291,0x0318,0x77a7,0x662e,0x54b5,0x453c,
-  0xbdcb,0xac42,0x9ed9,0x8f50,0xfbef,0xea66,0xd8fd,0xc974,
-  0x4204,0x538d,0x6116,0x709f,0x0420,0x15a9,0x2732,0x36bb,
-  0xce4c,0xdfc5,0xed5e,0xfcd7,0x8868,0x99e1,0xab7a,0xbaf3,
-  0x5285,0x430c,0x7197,0x601e,0x14a1,0x0528,0x37b3,0x263a,
-  0xdecd,0xcf44,0xfddf,0xec56,0x98e9,0x8960,0xbbfb,0xaa72,
-  0x6306,0x728f,0x4014,0x519d,0x2522,0x34ab,0x0630,0x17b9,
-  0xef4e,0xfec7,0xcc5c,0xddd5,0xa96a,0xb8e3,0x8a78,0x9bf1,
-  0x7387,0x620e,0x5095,0x411c,0x35a3,0x242a,0x16b1,0x0738,
-  0xffcf,0xee46,0xdcdd,0xcd54,0xb9eb,0xa862,0x9af9,0x8b70,
-  0x8408,0x9581,0xa71a,0xb693,0xc22c,0xd3a5,0xe13e,0xf0b7,
-  0x0840,0x19c9,0x2b52,0x3adb,0x4e64,0x5fed,0x6d76,0x7cff,
-  0x9489,0x8500,0xb79b,0xa612,0xd2ad,0xc324,0xf1bf,0xe036,
-  0x18c1,0x0948,0x3bd3,0x2a5a,0x5ee5,0x4f6c,0x7df7,0x6c7e,
-  0xa50a,0xb483,0x8618,0x9791,0xe32e,0xf2a7,0xc03c,0xd1b5,
-  0x2942,0x38cb,0x0a50,0x1bd9,0x6f66,0x7eef,0x4c74,0x5dfd,
-  0xb58b,0xa402,0x9699,0x8710,0xf3af,0xe226,0xd0bd,0xc134,
-  0x39c3,0x284a,0x1ad1,0x0b58,0x7fe7,0x6e6e,0x5cf5,0x4d7c,
-  0xc60c,0xd785,0xe51e,0xf497,0x8028,0x91a1,0xa33a,0xb2b3,
-  0x4a44,0x5bcd,0x6956,0x78df,0x0c60,0x1de9,0x2f72,0x3efb,
-  0xd68d,0xc704,0xf59f,0xe416,0x90a9,0x8120,0xb3bb,0xa232,
-  0x5ac5,0x4b4c,0x79d7,0x685e,0x1ce1,0x0d68,0x3ff3,0x2e7a,
-  0xe70e,0xf687,0xc41c,0xd595,0xa12a,0xb0a3,0x8238,0x93b1,
-  0x6b46,0x7acf,0x4854,0x59dd,0x2d62,0x3ceb,0x0e70,0x1ff9,
-  0xf78f,0xe606,0xd49d,0xc514,0xb1ab,0xa022,0x92b9,0x8330,
-  0x7bc7,0x6a4e,0x58d5,0x495c,0x3de3,0x2c6a,0x1ef1,0x0f78
-};
-
-
-
-static u16 crc(u8 *data, u8 len) {
-  u16 crc = 0;
-  for(int i=0; i < len; i++)
-      crc = (crc<<8) ^ CRCTable[((u8)(crc>>8) ^ *data++) & 0xFF];
-  return crc;
-}
+extern u16 frsky_CRCTable[];
+u16 frsky_crc(u8 *data, u8 len);
 
 static void initialize_data(u8 adr)
 {
@@ -143,8 +104,8 @@ static void initialize_data(u8 adr)
   CC2500_WriteReg(CC2500_07_PKTCTRL1,0x05);
 }
 
-
-static void set_start(u8 ch) {
+static void set_start(u8 ch)
+{
   CC2500_Strobe(CC2500_SIDLE);
   CC2500_WriteReg(CC2500_23_FSCAL3, calData[ch][0]);
   CC2500_WriteReg(CC2500_24_FSCAL2, calData[ch][1]);
@@ -160,7 +121,6 @@ static void redpine_build_bind_packet()
     packet[0] = 29;
     packet[1] = 0x03;
     packet[2] = 0x01;
-
     packet[3] = fixed_id;
     packet[4] = fixed_id >> 8;
     int idx = ((state - REDPINE_BIND) % 10) * 5;
@@ -175,7 +135,7 @@ static void redpine_build_bind_packet()
 
     memset(&packet[13], 0, packet_size_bind-15);
 
-    u16 lcrc = crc(&packet[3], packet_size_bind-5);
+    u16 lcrc = frsky_crc(&packet[3], packet_size_bind-5);
     packet[packet_size_bind-2] = lcrc >> 8;
     packet[packet_size_bind-1] = lcrc;
 }
@@ -220,11 +180,10 @@ enum {
     CHANNEL16
 };
 
+
 static void redpine_data_frame() {
     u16 chan[4];
  
-    ADC_Filter();
-    MIXER_CalcChannels();
     packet_size = Model.proto_opts[PROTO_OPTS_PACKETSIZE];
 
     memset(&packet[0], 0, packet_size);
@@ -254,9 +213,9 @@ static void redpine_data_frame() {
             | GET_FLAG(CHANNEL16, 0x80);
             
     packet[10] = Model.proto_opts[PROTO_OPTS_LOOPTIME]; 
-    //packet[11] = 0;  Future use
+    packet[11] = mixer_runtime/10;
 
-    u16 lcrc = crc(&packet[0], 12);
+    u16 lcrc = frsky_crc(&packet[0], 12);
     packet[12] = lcrc >> 8;
     packet[13] = lcrc;
 
@@ -266,43 +225,56 @@ static u16 redpine_cb() {
 
   switch(state) {
     default:
-      set_start(47);
-      CC2500_SetPower(Model.tx_power);
-      CC2500_Strobe(CC2500_SFRX);
-      redpine_build_bind_packet();
-      CC2500_Strobe(CC2500_SIDLE);
-      CC2500_WriteData(packet, packet[0]+1);
-      state++;
+        set_start(47);
+        CC2500_SetPower(Model.tx_power);
+        CC2500_Strobe(CC2500_SFRX);
+        redpine_build_bind_packet();
+        CC2500_Strobe(CC2500_SIDLE);
+        CC2500_WriteData(packet, packet[0]+1);
+        state++;
 #ifndef EMULATOR
-      return 9000;
+        return 9000;
 #else
-      return 90;
+        return 90;
 #endif
     case REDPINE_BIND_DONE:
-      PROTOCOL_SetBindState(0);
-      initialize_data(0);
-      channr = 0;
-      state++;
-      break;
-    case REDPINE_DATA1:
-      if (fine != (s8)Model.proto_opts[PROTO_OPTS_FREQFINE]) {
-          fine = (s8)Model.proto_opts[PROTO_OPTS_FREQFINE];
-          CC2500_WriteReg(CC2500_0C_FSCTRL0, fine);
-      }
-      CC2500_SetTxRxMode(TX_EN);
-      set_start(channr);
-      CC2500_SetPower(Model.tx_power);
-      CC2500_Strobe(CC2500_SFRX);
-      redpine_data_frame();
-      CC2500_Strobe(CC2500_SIDLE);
-      CC2500_WriteData(packet, packet[0]+1);
-      channr = (channr + 1) % 47;
-      state = REDPINE_DATA1;
+        PROTOCOL_SetBindState(0);
+        initialize_data(0);
+        channr = 0;
+        state++;
+break;
+
+    case REDPINE_DATAM:
 #ifndef EMULATOR
-      return (Model.proto_opts[PROTO_OPTS_LOOPTIME]*100);
+        CLOCK_RunMixer();    // clears mixer_sync, which is then set when mixer update complete
+        state = REDPINE_DATA1;
+        return mixer_runtime;
+#else
+        return 5;
+#endif
+          
+    case REDPINE_DATA1:
+        if (fine != (s8)Model.proto_opts[PROTO_OPTS_FREQFINE]) {
+            fine = (s8)Model.proto_opts[PROTO_OPTS_FREQFINE];
+            CC2500_WriteReg(CC2500_0C_FSCTRL0, fine);
+        }
+        CC2500_SetTxRxMode(TX_EN);
+        set_start(channr);
+        CC2500_SetPower(Model.tx_power);
+        CC2500_Strobe(CC2500_SFRX);
+        if (mixer_sync != MIX_DONE && mixer_runtime < 2000) { mixer_runtime += 1; }
+        redpine_data_frame();
+        CC2500_Strobe(CC2500_SIDLE);
+        CC2500_WriteData(packet, packet[0]+1);
+        channr = (channr + 1) % 47;
+        state = REDPINE_DATAM;
+#ifndef EMULATOR
+      return (Model.proto_opts[PROTO_OPTS_LOOPTIME]*100 - mixer_runtime);
 #else
       return Model.proto_opts[PROTO_OPTS_LOOPTIME];
 #endif
+
+
   }
   return 1;
 }
@@ -403,6 +375,7 @@ static int get_tx_id()
 static void initialize(int bind)
 {
     CLOCK_StopTimer();
+    mixer_runtime = 50;
 
     // initialize statics since 7e modules don't initialize
     fine = Model.proto_opts[PROTO_OPTS_FREQFINE];
