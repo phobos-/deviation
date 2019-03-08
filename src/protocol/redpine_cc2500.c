25bc8227a7a38abab536ea875ae81fce3138f3e3
Bryce Johnson <bryce@raspberrywood.com>
2019-03-08 10:12:27 -0700

    Added 'slow' mode - currently based off of radio params from frsky_x.  Good for some comparision distance testing

--- src/protocol/redpine_cc2500.c
+++ src/protocol/redpine_cc2500.c
@@ -35,17 +35,22 @@
 
 #include "iface_cc2500.h"
 
+static void initialize(int bind);
+static void redpine_init(unsigned int format);
+
 static const char * const redpine_opts[] = {
   _tr_noop("Freq-Fine"),  "-127", "127", NULL,
   _tr_noop("Format"),  "Fast", "Slow", NULL,
-  _tr_noop("LoopIn.1ms"),  "1", "250", NULL,
+  _tr_noop("Fast .1ms"),  "1", "250", NULL,
+  _tr_noop("Slow 1ms"),  "1", "250", NULL,
   _tr_noop("PacketSize"),  "14", "100", NULL,
   NULL
 };
 enum {
     PROTO_OPTS_FREQFINE,
     PROTO_OPTS_FORMAT,
-    PROTO_OPTS_LOOPTIME,
+    PROTO_OPTS_LOOPTIME_FAST,
+    PROTO_OPTS_LOOPTIME_SLOW,
     PROTO_OPTS_PACKETSIZE,
     LAST_PROTO_OPT,
 };
@@ -77,6 +82,8 @@ static u8 packet[MAX_PACKET_SIZE];
 static u16 mixer_runtime;
 
 static u8 hop_data[NUM_HOPS];
+
+unsigned format = 1; //todo: use enum
 
 
 static const u16 CRCTable[] = {
@@ -114,31 +121,29 @@ static const u16 CRCTable[] = {
   0x7bc7, 0x6a4e, 0x58d5, 0x495c, 0x3de3, 0x2c6a, 0x1ef1, 0x0f78
 };
 
-
-
-static u16 crc(u8 *data, u8 len) {
-  u16 crc = 0;
-  for (int i = 0; i < len; i++)
-      crc = (crc << 8) ^ CRCTable[((u8)(crc>>8) ^ *data++) & 0xFF];
-  return crc;
+static u16 crc(u8 *data, u8 len)
+{
+    u16 crc = 0;
+    for (int i = 0; i < len; i++)
+        crc = (crc << 8) ^ CRCTable[((u8)(crc >> 8) ^ *data++) & 0xFF];
+    return crc;
 }
 
 static void initialize_data(u8 adr)
 {
-  CC2500_WriteReg(CC2500_0C_FSCTRL0, fine);  // Frequency offset hack
-  CC2500_WriteReg(CC2500_18_MCSM0, 0x8);
-  CC2500_WriteReg(CC2500_09_ADDR, adr ? 0x03 : (fixed_id & 0xff));
-  CC2500_WriteReg(CC2500_07_PKTCTRL1, 0x05);
+    CC2500_WriteReg(CC2500_0C_FSCTRL0, fine); // Frequency offset hack
+    CC2500_WriteReg(CC2500_18_MCSM0, 0x8);
+    CC2500_WriteReg(CC2500_09_ADDR, adr ? 0x03 : (fixed_id & 0xff));
+    CC2500_WriteReg(CC2500_07_PKTCTRL1, 0x05);
 }
 
 static void set_start(u8 ch)
 {
-
-  CC2500_Strobe(CC2500_SIDLE);
-  CC2500_WriteReg(CC2500_23_FSCAL3, calData[ch][0]);
-  CC2500_WriteReg(CC2500_24_FSCAL2, calData[ch][1]);
-  CC2500_WriteReg(CC2500_25_FSCAL1, calData[ch][2]);
-  CC2500_WriteReg(CC2500_0A_CHANNR, hop_data[ch]);
+    CC2500_Strobe(CC2500_SIDLE);
+    CC2500_WriteReg(CC2500_23_FSCAL3, calData[ch][0]);
+    CC2500_WriteReg(CC2500_24_FSCAL2, calData[ch][1]);
+    CC2500_WriteReg(CC2500_25_FSCAL1, calData[ch][2]);
+    CC2500_WriteReg(CC2500_0A_CHANNR, hop_data[ch]);
 }
 
 #define RXNUM 16
@@ -168,17 +173,12 @@ static void redpine_build_bind_packet()
     packet[packet_size_bind-1] = lcrc;
 }
 
-#define STICK_SCALE    751
-static u16 scaleForPXX(u8 chan)
+static u16 scaleForRedpine(u8 chan)
 {
-// mapped 860,2140(125%) range to 64,1984(PXX values);
-// return (u16)(((Servo_data[i]-PPM_MIN)*3)>>1)+64;
-// 0-2047, 0 = 817, 1024 = 1500, 2047 = 2182
     s32 chan_val;
 
-    chan_val = Channels[chan];
-    chan_val = chan_val * STICK_SCALE / CHAN_MAX_VALUE + 1024;
-
+    chan_val = Channels[chan] * 15 * 100 / (2 * CHAN_MAX_VALUE) + 1024;
+    
     if (chan_val > 2046)   chan_val = 2046;
     else if (chan_val < 1) chan_val = 1;
 
@@ -219,10 +219,10 @@ static void redpine_data_frame() {
     packet[1] = fixed_id;
     packet[2] = fixed_id >> 8;
 
-    chan[0] = scaleForPXX(0);
-    chan[1] = scaleForPXX(1);
-    chan[2] = scaleForPXX(2);
-    chan[3] = scaleForPXX(3);
+    chan[0] = scaleForRedpine(0);
+    chan[1] = scaleForRedpine(1);
+    chan[2] = scaleForRedpine(2);
+    chan[3] = scaleForRedpine(3);
 
     packet[3] = chan[0];
     packet[4] = (((chan[0] >> 8) & 0x07) | (chan[1] << 4)) | GET_FLAG(CHANNEL5, 0x08);
@@ -239,7 +239,12 @@ static void redpine_data_frame() {
             | GET_FLAG(CHANNEL15, 0x40)
             | GET_FLAG(CHANNEL16, 0x80);
 
-    packet[10] = Model.proto_opts[PROTO_OPTS_LOOPTIME];
+    if (Model.proto_opts[PROTO_OPTS_FORMAT] == 0) {
+        packet[10] = Model.proto_opts[PROTO_OPTS_LOOPTIME_FAST];
+    } else {
+        packet[10] = Model.proto_opts[PROTO_OPTS_LOOPTIME_SLOW];
+    }
+    
     packet[11] = mixer_runtime/10;
 
     u16 lcrc = crc(&packet[0], 12);
@@ -250,6 +255,12 @@ static void redpine_data_frame() {
 static u16 redpine_cb() {
   switch (state) {
     default:
+        if (state == REDPINE_BIND) {
+            redpine_init(0);
+        }
+        if (state == REDPINE_BIND_DONE/2) {
+           redpine_init(1);
+        }
         set_start(49);
         CC2500_SetPower(Model.tx_power);
         CC2500_Strobe(CC2500_SFRX);
@@ -283,20 +294,37 @@ break;
             fine = (s8)Model.proto_opts[PROTO_OPTS_FREQFINE];
             CC2500_WriteReg(CC2500_0C_FSCTRL0, fine);
         }
+        if (format != (unsigned)Model.proto_opts[PROTO_OPTS_FORMAT]) {
+            format = (unsigned)Model.proto_opts[PROTO_OPTS_FORMAT];
+            redpine_init(format);
+            mixer_runtime = 50;
+            return 5000;
+        }
+
         CC2500_SetTxRxMode(TX_EN);
         set_start(channr);
         CC2500_SetPower(Model.tx_power);
         CC2500_Strobe(CC2500_SFRX);
-        if (mixer_sync != MIX_DONE && mixer_runtime < 2000) { mixer_runtime += 1; }
+        if (mixer_sync != MIX_DONE && mixer_runtime < 2000) { 
+            mixer_runtime += 1; 
+        }
         redpine_data_frame();
         CC2500_Strobe(CC2500_SIDLE);
         CC2500_WriteData(packet, packet[0]+1);
         channr = (channr + 1) % 49;
         state = REDPINE_DATAM;
 #ifndef EMULATOR
-      return (Model.proto_opts[PROTO_OPTS_LOOPTIME]*100 - mixer_runtime);
+        if (Model.proto_opts[PROTO_OPTS_FORMAT] == 0) {
+            return (Model.proto_opts[PROTO_OPTS_LOOPTIME_FAST]*100 - mixer_runtime);
+        } else {
+            return (Model.proto_opts[PROTO_OPTS_LOOPTIME_SLOW]*1000 - mixer_runtime);
+        }
 #else
-      return Model.proto_opts[PROTO_OPTS_LOOPTIME];
+        if (Model.proto_opts[PROTO_OPTS_FORMAT] == 0) {
+            return (Model.proto_opts[PROTO_OPTS_LOOPTIME_FAST]);
+        } else {
+            return (Model.proto_opts[PROTO_OPTS_LOOPTIME_SLOW]);
+        }
 #endif
   }
   return 1;
@@ -304,57 +332,57 @@ break;
 
 // register, fast 250k, slow
 static const u8 init_data[][3] = {
-    {CC2500_00_IOCFG2,    0x06,  0x06},
-    {CC2500_02_IOCFG0,    0x06,  0x06},
-    {CC2500_06_PKTLEN,    0x1E,  0x23},
-    {CC2500_07_PKTCTRL1,  0x04,  0x04},
-    {CC2500_08_PKTCTRL0,  0x05,  0x01},
-    {CC2500_0B_FSCTRL1,   0x0A,  0x08},
-    {CC2500_0C_FSCTRL0,   0x00,  0x00},
-    {CC2500_0D_FREQ2,     0x5D,  0x5c},
-    {CC2500_0E_FREQ1,     0x93,  0x80},
-    {CC2500_0F_FREQ0,     0xB1,  0x00},
-    {CC2500_10_MDMCFG4,   0x2D,  0x7B},
-    {CC2500_11_MDMCFG3,   0x3B,  0xF8},
-    {CC2500_12_MDMCFG2,   0x73,  0x03},
-    {CC2500_13_MDMCFG1,   0x22,  0x23},
-    {CC2500_14_MDMCFG0,   0xF8,  0x7a},
-    {CC2500_15_DEVIATN,   0x00,  0x53},
-    {CC2500_17_MCSM1,     0x0c,  0x0E},
-    {CC2500_18_MCSM0,     0x18,  0x18},
-    {CC2500_3E_PATABLE,   0xff,  0xff},
+    {CC2500_00_IOCFG2,    0x06, 0x06},
+    {CC2500_02_IOCFG0,    0x06, 0x06},    
+    {CC2500_03_FIFOTHR,   0x07, 0x07},
+    {CC2500_06_PKTLEN,    0x1E, 0x1E},  
+    {CC2500_07_PKTCTRL1,  0x04, 0x04},
+    {CC2500_08_PKTCTRL0,  0x01, 0x01},      
+    {CC2500_09_ADDR,      0x00, 0x00},
+    {CC2500_0B_FSCTRL1,   0x0A, 0x0A},    
+    {CC2500_0C_FSCTRL0,   0x00, 0x00},
+    {CC2500_0D_FREQ2,     0x5D, 0x5c},
+    {CC2500_0E_FREQ1,     0x93, 0x76},
+    {CC2500_0F_FREQ0,     0xB1, 0x27},
+    {CC2500_10_MDMCFG4,   0x2D, 0x7B},
+    {CC2500_11_MDMCFG3,   0x3B, 0x61},
+    {CC2500_12_MDMCFG2,   0x73, 0x13},
+    {CC2500_13_MDMCFG1,   0x23, 0x23},
+    {CC2500_14_MDMCFG0,   0x56, 0x7a}, //Chan space
+    {CC2500_15_DEVIATN,   0x00, 0x51},        
+    {CC2500_17_MCSM1,     0x0c, 0x0c},
+    {CC2500_18_MCSM0,     0x18, 0x18},
+    {CC2500_19_FOCCFG,    0x1D, 0x16},
+    {CC2500_1A_BSCFG,     0x1C, 0x6c},
+    {CC2500_1B_AGCCTRL2,  0xC7, 0x43},
+    {CC2500_1C_AGCCTRL1,  0x00, 0x40},
+    {CC2500_1D_AGCCTRL0,  0xB0, 0x91},   
+    {CC2500_21_FREND1,    0xB6, 0x56},  
+    {CC2500_22_FREND0,    0x10, 0x10},
+    {CC2500_23_FSCAL3,    0xEA, 0xA9},  
+    {CC2500_24_FSCAL2,    0x0A, 0x0A},
+    {CC2500_25_FSCAL1,    0x00, 0x00},
+    {CC2500_26_FSCAL0,    0x11, 0x11},           
+    {CC2500_29_FSTEST,    0x59, 0x59},
+    {CC2500_2C_TEST2,     0x88, 0x88},
+    {CC2500_2D_TEST1,     0x31, 0x31},
+    {CC2500_2E_TEST0,     0x0B, 0x0B},
 };
 
-// register, value
 static const u8 init_data_shared[][2] = {
-    {CC2500_03_FIFOTHR,   0x07},
-    {CC2500_09_ADDR,      0x00},
-    {CC2500_19_FOCCFG,    0x1D},
-    {CC2500_1A_BSCFG,     0x1C},
-    {CC2500_1B_AGCCTRL2,  0xC7},
-    {CC2500_1C_AGCCTRL1,  0x00},
-    {CC2500_1D_AGCCTRL0,  0xB0},
-    {CC2500_21_FREND1,    0xB6},
-    {CC2500_22_FREND0,    0x10},
-    {CC2500_23_FSCAL3,    0xa9},
-    {CC2500_24_FSCAL2,    0x0A},
-    {CC2500_25_FSCAL1,    0x00},
-    {CC2500_26_FSCAL0,    0x11},
-    {CC2500_29_FSTEST,    0x59},
-    {CC2500_2C_TEST2,     0x88},
-    {CC2500_2D_TEST1,     0x31},
-    {CC2500_2E_TEST0,     0x0B},
+    {CC2500_3E_PATABLE,   0xff}
 };
 
-static void redpine_init() {
+
+static void redpine_init(unsigned int format) {
   CC2500_Reset();
 
-  unsigned format = Model.proto_opts[PROTO_OPTS_FORMAT] + 1;
-
-  for (unsigned i=0; i < ((sizeof init_data) / (sizeof init_data[0])); i++)
-      CC2500_WriteReg(init_data[i][0], init_data[i][format]);
-  for (unsigned i=0; i < ((sizeof init_data_shared) / (sizeof init_data_shared[0])); i++)
+  for (unsigned i=0; i < ((sizeof init_data) / (sizeof init_data[0])); i++) {
+      CC2500_WriteReg(init_data[i][0], init_data[i][format+1]);
+  }
+  for (unsigned i=0; i < ((sizeof init_data_shared) / (sizeof init_data_shared[0])); i++) {
       CC2500_WriteReg(init_data_shared[i][0], init_data_shared[i][1]);
+  }
 
   CC2500_WriteReg(CC2500_0C_FSCTRL0, fine);
   CC2500_Strobe(CC2500_SIDLE);
@@ -371,8 +399,6 @@ static void redpine_init() {
   }
 }
 
-
-
 // Generate internal id from TX id and manufacturer id (STM32 unique id)
 static int get_tx_id()
 {
@@ -383,8 +409,9 @@ static int get_tx_id()
     for (int i = 0; i < 12; ++i) {
         rand32_r(&lfsr, var[i]);
     }
-    for (u8 i = 0, j = 0; i < sizeof(Model.fixed_id); ++i, j += 8)
+    for (u8 i = 0, j = 0; i < sizeof(Model.fixed_id); ++i, j += 8) {
         rand32_r(&lfsr, (Model.fixed_id >> j) & 0xff);
+    }
     return rand32_r(&lfsr, 0);
 }
 
@@ -406,21 +433,25 @@ static void initialize(int bind)
     hop_data[idx++] = 1;
     while (idx < sizeof(hop_data)-1) {
         u32 i;
-        rnd = rnd * 0x0019660D + 0x3C6EF35F; // Randomization
+        rnd = rnd * 0x0019660D + 0x3C6EF35F;  // Randomization
         // Drop least-significant byte for better randomization. Start from 1
         u8 next_ch = (rnd >> 8) % MAX_RF_CHANNEL + 1;
         // Check that it's not duplicate nor adjacent nor channel 0 or 1
         for (i = 0; i < idx; i++) {
             u8 ch = hop_data[i];
-            if( (ch <= next_ch + 1) && (ch >= next_ch - 1) && (ch > 1) ) break;
+            if ((ch <= next_ch + 1) && (ch >= next_ch - 1) && (ch > 1)) {
+                break;
+            }
         }
-        if (i != idx)
+        if (i != idx) {
             continue;
+        }
         hop_data[idx++] = next_ch;
     }
-    hop_data[49] = 0; //Last channel is the bind channel at hop 0
+    hop_data[49] = 0;  //Last channel is the bind channel at hop 0
 
-    redpine_init();
+    redpine_init(format);
+
     CC2500_SetTxRxMode(TX_EN);  // enable PA
 
     if (bind) {
@@ -449,7 +480,12 @@ uintptr_t REDPINE_Cmds(enum ProtoCmds cmd)
         case PROTOCMD_DEFAULT_NUMCHAN: return 16L;
         case PROTOCMD_CURRENT_ID: return Model.fixed_id;
         case PROTOCMD_GETOPTIONS:
-            if (!Model.proto_opts[PROTO_OPTS_LOOPTIME]) Model.proto_opts[PROTO_OPTS_LOOPTIME] = 15;  // if not set, default to no gain
+            if (!Model.proto_opts[PROTO_OPTS_LOOPTIME_FAST]) {
+                Model.proto_opts[PROTO_OPTS_LOOPTIME_FAST] = 20;  // if not set, default to no gain
+            }
+            if (!Model.proto_opts[PROTO_OPTS_LOOPTIME_SLOW]) {
+                Model.proto_opts[PROTO_OPTS_LOOPTIME_SLOW] = 9;  // if not set, default to no gain
+            }
             return (uintptr_t)redpine_opts;
         case PROTOCMD_RESET:
         case PROTOCMD_DEINIT:
